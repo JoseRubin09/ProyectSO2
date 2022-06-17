15bd82f375fce4f6b9c9e373901c0a21e1497c43
Jose Rubin <jmanuelrubin1@gmail.com>
2022-06-17 17:03:54 -0400

    first commit

---
+++ src/Classes/main.java
@@ -0,0 +1,22 @@
+package Classes;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jose Rubin
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
